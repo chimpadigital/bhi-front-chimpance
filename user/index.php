1f61b043ea32b15bff497efe1326caa5a5220515
chimpadigital <sdesigncba@gmail.com>
2021-03-17 12:52:47 -0300

    assets head

--- user/index.php
+++ user/index.php
@@ -5,10 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BHI</title>
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
-    <link rel="stylesheet" href="assets/css/responsive.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/bootstrap-icons.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap.css">
+    <link rel="stylesheet" href="../assets/css/responsive.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap-icons.css">
     <!-- <link rel="stylesheet" href="/assets/css/bootstrap-reboot.css"> -->
     <!-- <link rel="stylesheet" href="css/bootstrap-utilities.css"> -->
     
